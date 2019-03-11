3b233ad7d8ba57aba420501da249fe6d3a082b2e
Qpesa <46829719+Qpesa@users.noreply.github.com>
2019-03-11 23:07:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var api = 'http://134.209.17.160:15341';
-var donationAddress = "";
+var donationAddress = "QPSATsp3pDuHRF8ex8mdij4Sf1tFQjTdm3JneLkZdovw9VrgKaVtQqrbahczFdrEkRb8cCNhPP1LEeW9kpyo1AS42choKW6cHm";
 var blockTargetInterval = 120; // enter the block interval in seconds
 var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
 var totalSupply =  2300000000000000; // enter the total supply in atomic units
@@ -8,25 +8,13 @@ var refreshDelay = 30000;
 
 // pools stats by MainCoins
 var networkStat = {
- "trtl": [
-	["z-pool.com", "http://z-pool.com:8117"],
-	["eu.turtlepool.space", "http://eu.turtlepool.space:8117"],
-	["us.turtlepool.space", "http://us.turtlepool.space:8117"],
-	["hk.turtlepool.space", "http://hk.turtlepool.space:8117"],
-	["turtlecoinpool.ddns.net", "http://turtlecoinpool.ddns.net:8127"],
-	["trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
-	["trtl.heigh-ho.funkypenguin.co.nz", "https://api.trtl.heigh-ho.funkypenguin.co.nz"],
-	["trtl.radicaldelta.org", "http://trtl.radicaldelta.org:8117"],
-	["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
-	["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
-	["xk.is", "https://xk.is/api"],
-	["trtl.llama.horse","http://pool.llama.horse:8118"],
-	["trtl.hackerknowledge.de", "https://pool.trtl.hackerknowledge.de:8119"]
+ "QPSA": [
+	
  ]
 };
 
 var networkStat2 = {
-    "trtl": [
+    "QPSA": [
 	[""]
  ]
 };
