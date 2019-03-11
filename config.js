d36a4085e4c45c85e6c14519ce8d9b2732db6a2f
Qpesa <46829719+Qpesa@users.noreply.github.com>
2019-03-12 00:03:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://134.209.17.160:15341';
+var api = 'http://159.203.116.216:15341';
 var donationAddress = "QPSATsp3pDuHRF8ex8mdij4Sf1tFQjTdm3JneLkZdovw9VrgKaVtQqrbahczFdrEkRb8cCNhPP1LEeW9kpyo1AS42choKW6cHm";
 var blockTargetInterval = 120; // enter the block interval in seconds
 var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
