c5f87cd10e0e804869840717d5f6dddf03c9489a
Qpesa <46829719+Qpesa@users.noreply.github.com>
2019-04-25 18:43:27 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-var api = 'http://134.209.52.211:15341';
-var donationAddress = "QPSATsp3pDuHRF8ex8mdij4Sf1tFQjTdm3JneLkZdovw9VrgKaVtQqrbahczFdrEkRb8cCNhPP1LEeW9kpyo1AS42choKW6cHm";
-var blockTargetInterval = 120; // enter the block interval in seconds
+var api = 'http://134.209.52.211:16920';
+var donationAddress = "QPSAfh3Z7b3ae6fuLvRLHMXkNdESaC2aaZTsY3gAcdBKJCWjE629X4hG7KP9u3tHujC1J1QbX32sePu2EHHncjUW7HsPz2bFZC";
+var blockTargetInterval = 60; // enter the block interval in seconds
 var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
-var totalSupply =  2300000000000000; // enter the total supply in atomic units
+var totalSupply =  50000000000000000; // enter the total supply in atomic units
 var symbol = 'QPSA'; // enter the coin's ticker
 var refreshDelay = 30000;
 
