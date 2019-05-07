1f481809fefedc8a4f6f479f1ac0cd67fec6aadf
Qpesa <46829719+Qpesa@users.noreply.github.com>
2019-05-07 18:29:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var api = 'http://104.248.79.64:28675';
 var donationAddress = "QPSAfh3Z7b3ae6fuLvRLHMXkNdESaC2aaZTsY3gAcdBKJCWjE629X4hG7KP9u3tHujC1J1QbX32sePu2EHHncjUW7HsPz2bFZC";
 var blockTargetInterval = 60; // enter the block interval in seconds
 var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
-var totalSupply =  50000000000000000; // enter the total supply in atomic units
+var totalSupply =  100000000000000000; // enter the total supply in atomic units
 var symbol = 'QPSA'; // enter the coin's ticker
 var refreshDelay = 30000;
 
